f5d5764deb434698db42c69743eff20561641af9
EfecanE <mh.efecanefe@gmail.com>
2023-05-06 10:50:47 +0300

    'added Card component and styled homepage'

---
+++ components/card.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import cls from "classnames";
+
+import styles from "./card.module.css";
+
+const Card = ({ imgUrl, name, id }) => {
+  return (
+    <Link className={styles.cardLink} href={`/coffee-store/${id}`}>
+      <div className={cls("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{name}</h2>
+        </div>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            className={styles.cardImage}
+            src={imgUrl}
+            alt={name}
+            width={260}
+            height={160}
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import { useState } from "react";
 import Banner from "@/components/banner";
+import Card from "@/components/card";
 
+import coffeeData from "@/public/static/coffee-stores.json";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-
   const handleOnBannerBtnClick = () => {
     console.log("clicked");
-    setLoading(true);
   };
 
   return (
@@ -21,9 +19,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <header className={styles.main}>
         <Banner
-          buttonText={loading ? "Loading" : "View shops nearby"}
+          buttonText={"View shops nearby"}
           buttonOnClick={handleOnBannerBtnClick}
         />
         <div className={styles.imageWrapper}>
@@ -34,6 +32,18 @@ export default function Home() {
             height={256}
           ></Image>
         </div>
+      </header>
+      <main className={styles.cardWrapper}>
+        {coffeeData.map((coffeeShop) => {
+          return (
+            <Card
+              key={coffeeShop.id}
+              name={coffeeShop.name}
+              imgUrl={coffeeShop.imgUrl}
+              id={coffeeShop.id}
+            />
+          );
+        })}
       </main>
     </>
   );
